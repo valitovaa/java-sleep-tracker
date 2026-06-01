5fb443768e112acba882e0bf461eb37857d4ab46
Dilara Valitova <valitovadilara@icloud.com>
2026-06-01 15:51:56 +0300

    deleted unused import

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepingPersonalityClassificatorTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepingPersonalityClassificatorTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.sleeptracker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
